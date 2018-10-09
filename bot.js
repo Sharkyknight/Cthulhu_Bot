910fceda281eeec53b47c4a542b4dad309ef8a32
Kirsty <HallowedRedeemer@users.noreply.github.com>
2018-10-09 15:49:40 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,18 +25,17 @@ user1 = "Sharkyknight";
 *   Randomly selected value within obj, where all {0}s are replaced with name
 */
 function randomDialogue(obj, name){
-    message.channel.sendMessage("Sharky trust me it'll be okay");
     if (typeof name === "undefined" || !obj || obj == "null" || object == "undefined") {    // Parameters incorrect
         return("Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.");
     }
     else {
-        var num = Object.keys(dialogues.poem).length;                  // Total num of dialogues available
-        var rng = Math.floor(Math.random()*num);
-        var dialog = dialogues.poem[rng];
-        while (dialog.includes("{0}")){                     // insert name into poem
-            dialog = dialog.replace("{0}", name);
-        }
-        return dialog;
+        //var num = Object.keys(dialogues.poem).length;                  // Total num of dialogues available
+        //var rng = Math.floor(Math.random()*num);
+        //var dialog = dialogues.poem[rng];
+        //while (dialog.includes("{0}")){                     // insert name into poem
+        //    dialog = dialog.replace("{0}", name);
+        //}
+        return "It'll be okay, trust me Sharky";
     }
 }
     
