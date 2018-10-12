c3d43a624a9622c7733e7a312d541cc023ae5da6
Kirsty <HallowedRedeemer@users.noreply.github.com>
2018-10-12 16:48:57 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ bot.on('message', message => {
     }*/
     
     if (!message.content.startsWith(PREFIX)) {
-        if (message.author.username === "Carsomyr"){
+        if (message.author.username === "Carsomyr" && message.channel.name == "general"){
             message.channel.sendMessage("TestA");
             if (message.attachments.size > 0){
                 message.channel.sendMessage("TestB");
