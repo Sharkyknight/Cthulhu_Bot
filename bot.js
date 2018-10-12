6f57bfe9481fc85311cff1f6d0263568c3724b59
Kirsty <HallowedRedeemer@users.noreply.github.com>
2018-10-12 19:50:41 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,7 +117,8 @@ bot.on('message', message => {
             var clock = getClock();
             var msg = messages.clock.replace("{0}", clock.toTimeString());
             msg = msg.slice(0, -25);
-            message.channel.sendMessage(clock.toDateString + " " + msg + " Game Time");
+            msg = clock.toString + " " + msg;
+            message.channel.sendMessage(msg + " Game Time");
             break;
         case "reboot":
             var cDate = getClock(); // Current Date
