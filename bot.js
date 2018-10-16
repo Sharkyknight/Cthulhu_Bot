2c50bdec6edaa011c50d64e3dda4699fe2d07098
Kirsty <HallowedRedeemer@users.noreply.github.com>
2018-10-16 21:20:18 +1300

    Actually respond in channel

--- bot.js
+++ bot.js
@@ -115,11 +115,15 @@ bot.on('message', message => {
                 message.channel.sendMessage(messages.rumfail);
                 break;
             }
+            message.channel.sendMessage("Test A");
             swill = swill * alcohol[0];
             grog = grog * alcohol[1];
             rum = rum * alcohol[2];
+            message.channel.sendMessage("Test B");
             var cheapest = (rum < grog && rum < swill) ? "rum" : (grog < swill) ? "grog" : "swill";
+            message.channel.sendMessage("Test C");
             var response = messages.rum.replace("{0}", rum, "{1}", grog, "{2}", swill, "{3}", cheapest);
+            message.channel.sendMessage(response);
             break;
         case "labor":       // Find required labour cost to cover badge cost
             var amount = parseInt(commands[1], 10);
