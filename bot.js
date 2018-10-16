0e760f9a2db9866128182ac0f2cff2ba82eedf5b
Kirsty <HallowedRedeemer@users.noreply.github.com>
2018-10-16 20:48:21 +1300

    Add try catch to rum calc

--- bot.js
+++ bot.js
@@ -104,11 +104,16 @@ bot.on('message', message => {
         // Commands
         case "rum":     // Calculate which alcohol is the best value
             var alcohol = [15, 10, 6];   // Swill, grog, rum
-            var swill = alcohol[0] * parseInt(commands[1], 10);
-            var grog = alcohol[1] * parseInt(commands[2], 10);
-            var rum = alcohol[2] * parseInt(commands[3], 10);
-            if (swill < 1 || grog < 1 || rum < 1){
+            try {
+                var swill = alcohol[0] * parseInt(commands[1], 10);
+                var grog = alcohol[1] * parseInt(commands[2], 10);
+                var rum = alcohol[2] * parseInt(commands[3], 10);
+                if (swill < 1 || grog < 1 || rum < 1){
+                    throw ("Invalid numbers.")
+                }
+            } catch (e){
                 message.channel.sendMessage(messages.rumfail);
+                break;
             }
             var cheapest = (rum < grog && rum < swill) ? "rum" : (grog < swill) ? "grog" : "swill";
             var response = messages.rum.replace("{0}", rum, "{1}", grog, "{2}", swill, "{3}", cheapest);
